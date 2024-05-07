af1dd1411de621b2191beef431e616665fec2e61
cstroh23 <christopher.stroh@stud.fh-campuswien.ac.at>
2024-05-07 20:28:13 +0200

    Exception Logic added, Data Layer still not working correctly

--- src/main/java/at/ac/fhcampuswien/fhmdb/exceptions/MovieAPIException.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/exceptions/MovieAPIException.java
@@ -1,4 +1,19 @@
 package at.ac.fhcampuswien.fhmdb.exceptions;
 
-public class MovieAPIException {
+public class MovieAPIException extends Exception{
+    public MovieApiException() {
+        super();
+    }
+
+    public MovieApiException(String message) {
+        super(message);
+    }
+
+    public MovieApiException(Throwable cause) {
+        super(cause);
+    }
+
+    public MovieApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
